41641d63df04c67a49d287791f92601f8ccfb556
bluejblue <bluejbluethefourth@gmail.com>
2025-02-08 13:43:39 -0500

    Led Animation Command added.

---
+++ src/main/java/frc/robot/commands/AutoCommands/LedAnimationCommand.java
@@ -0,0 +1,49 @@
+package frc.robot.commands.AutoCommands;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.not_drive.ledSubsystem;
+
+public class LedAnimationCommand extends Command {
+    private ledSubsystem subsystem;
+    private boolean isEndGame = false;
+
+    public LedAnimationCommand(ledSubsystem subsystem) {
+        this.subsystem = subsystem;
+        addRequirements(subsystem);
+}
+
+@Override
+public boolean runsWhenDisabled() {
+    return true;
+}
+
+@Override
+public void initialize() {
+    // Initialize LED behavior here
+}
+
+@Override
+public void execute() {
+    double matchTime = DriverStation.getMatchTime();
+
+    if (matchTime <= 30 && matchTime != -1) {
+        isEndGame = true;
+        subsystem.setColor(255, 0, 0);  // Red color during Endgame
+    } else {
+        isEndGame = false;
+        subsystem.setColor(0, 0, 255);  // Blue color during normal play
+    }
+}
+
+@Override
+public boolean isFinished() {
+    return false;
+}
+
+@Override
+public void end(boolean interrupted) {
+    // Optionally turn off LEDs when the command ends
+    subsystem.TurnOffLEDs();
+}
+}
