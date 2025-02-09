0ab22720f817f276df0f97fb66ee5af728ebb02c
bluejblue <bluejbluethefourth@gmail.com>
2025-02-09 14:01:11 -0500

    Comment change

--- src/main/java/frc/robot/commands/AutoCommands/LedAnimationCommand.java
+++ src/main/java/frc/robot/commands/AutoCommands/LedAnimationCommand.java
@@ -5,45 +5,46 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.not_drive.ledSubsystem;
 
 public class LedAnimationCommand extends Command {
-    private ledSubsystem subsystem;
-    private boolean isEndGame = false;
+  private ledSubsystem subsystem;
+  private boolean isEndGame = false;
 
-    public LedAnimationCommand(ledSubsystem subsystem) {
-        this.subsystem = subsystem;
-        addRequirements(subsystem);
-}
+  public LedAnimationCommand(ledSubsystem subsystem) {
+    this.subsystem = subsystem;
+    addRequirements(subsystem);
+  }
 
-@Override
-public boolean runsWhenDisabled() {
+  @Override
+  public boolean runsWhenDisabled() {
     return true;
-}
+  }
 
-@Override
-public void initialize() {
+  @Override
+  public void initialize() {
     // Initialize LED behavior here
-}
+  }
 
-@Override
-public void execute() {
+  @Override
+  public void execute() {
     double matchTime = DriverStation.getMatchTime();
 
     if (matchTime <= 30 && matchTime != -1) {
-        isEndGame = true;
-        subsystem.setColor(255, 0, 0);  // Red color during Endgame
+      isEndGame = true;
+      subsystem.setColor(255, 0, 0); // Red color during Endgame
     } else {
-        isEndGame = false;
-        subsystem.setColor(0, 0, 255);  // Blue color during normal play
+      isEndGame = false;
+      subsystem.setColor(0, 0, 255); // Blue color during normal play
     }
-}
+  }
 
-@Override
-public boolean isFinished() {
+  @Override
+  public boolean isFinished() {
     return false;
-}
+  }
 
-@Override
-public void end(boolean interrupted) {
+  @Override
+  public void end(boolean interrupted) {
     // Optionally turn off LEDs when the command ends
     subsystem.TurnOffLEDs();
+  }
 }
-}
+// Need to look at this so see if correct
