c17e4361245c5fc6514eb7f8c9296752b3c0ed5d
Carter Earles <carterearles07@gmail.com>
2025-02-24 19:43:38 -0500

    Found the april tag field loading thing

--- src/main/java/frc/robot/util/AprilTagStuff/FieldInfo.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.util.AprilTagStuff;
-
-public class FieldInfo{
-    public float fieldlength;
-    public float fieldwidth;
-    public AprilTagInfo[] fiducials;
-    public String type;
-}
